f01838795cee9d528ee85bda9141fea1fac0f490
lucyaguilar <lucywaldina12@gmail.com>
2022-07-12 17:42:31 -0600

    Agregando validaciones a productos y categorias e imagenes a productos

--- GameStop/GameStop.BL/ProductoBL.cs
+++ GameStop/GameStop.BL/ProductoBL.cs
@@ -18,8 +18,46 @@ namespace GameStop.BL
         }
         public List<Producto> ObtenerProductos()
         {
-            ListadeProductos = _contexto.Productos.ToList();
+            ListadeProductos = _contexto.Productos
+                .Include("Categoria")
+                    .ToList();
             return _contexto.Productos.ToList();
         }
+
+        public void GuardarProducto(Producto producto)
+        {
+            if (producto.Id == 0)
+            {
+                _contexto.Productos.Add(producto);
+            }
+            else
+            {
+                var productoExistente = _contexto.Productos.Find(producto.Id);
+                productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.CategoriaId = producto.CategoriaId;
+                productoExistente.Precio = producto.Precio;
+                productoExistente.UrlImagen = producto.UrlImagen;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Producto ObtenerProducto(int id)
+        {
+            var producto = _contexto.Productos
+                .Include("Categoria").FirstOrDefault(p=> p.Id == id);
+
+            return producto ;
+        }
+
+        public void EliminarProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+
+            _contexto.Productos.Remove(producto);
+            _contexto.SaveChanges();
+
+        }
+
     }
 }
